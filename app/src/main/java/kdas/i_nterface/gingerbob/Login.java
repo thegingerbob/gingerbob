7b86008bd96e8166aa77acb29ecc6fffdb655c86
das11 <kabird1196@gmail.com>
2018-03-04 00:30:41 +0530

    INIT

---
+++ app/src/main/java/kdas/i_nterface/gingerbob/Login.java
@@ -0,0 +1,13 @@
+package kdas.i_nterface.gingerbob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
